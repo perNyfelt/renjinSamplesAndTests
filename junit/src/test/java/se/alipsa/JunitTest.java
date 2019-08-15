7b7330571336c6d08464c0b6ba0acd7b36d8b4b0
Per Nyfelt <per.nyfelt@alipsa.se>
2019-08-15 19:31:41 +0200

    cleanup

--- junit/src/test/java/se/alipsa/JunitTest.java
+++ junit/src/test/java/se/alipsa/JunitTest.java
@@ -1,18 +1,27 @@
 package se.alipsa;
 
 import org.junit.Test;
+import org.renjin.eval.EvalException;
 import org.renjin.script.RenjinScriptEngine;
 import org.renjin.script.RenjinScriptEngineFactory;
 
 import javax.script.ScriptException;
 
+import static org.junit.Assert.fail;
+
 public class JunitTest {
 
    @Test
-   public void TestJunit() throws ScriptException {
+   public void TestJunit() {
       RenjinScriptEngineFactory renjinScriptEngineFactory = new RenjinScriptEngineFactory();
       RenjinScriptEngine engine = renjinScriptEngineFactory.getScriptEngine();
-      engine.eval("a <- 24; print(a)");
+      try {
+         engine.eval("a <- 24; print(a)");
+      } catch (ScriptException | EvalException e) {
+         System.err.println("Failed to execute script");
+         e.printStackTrace();
+         fail(e.toString());
+      }
    }
 
 }
---
+++ junit/src/test/java/se/alipsa/SimpleRenjinWithDBITest.java
@@ -0,0 +1,68 @@
+package se.alipsa;
+
+import org.junit.Test;
+import org.renjin.eval.EvalException;
+import org.renjin.script.RenjinScriptEngine;
+import org.renjin.script.RenjinScriptEngineFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.script.ScriptException;
+
+import static org.junit.Assert.fail;
+
+public class SimpleRenjinWithDBITest {
+
+   Logger log = LoggerFactory.getLogger(SimpleRenjinWithDBITest.class);
+
+   @Test
+   public void testSimpleDBI() {
+      RenjinScriptEngineFactory factory = new RenjinScriptEngineFactory();
+      RenjinScriptEngine engine = factory.getScriptEngine();
+      StringBuilder str = new StringBuilder()
+         .append("library('org.renjin.cran:DBI')\n")
+         .append("library('org.renjin:hamcrest')\n")
+         .append("a <- 24; print(a)");
+      try {
+         engine.eval(str.toString());
+      } catch (ScriptException | EvalException e) {
+         System.err.println("Failed to execute script");
+         e.printStackTrace();
+         fail(e.toString());
+      }
+   }
+
+
+   @Test
+   public void testPlotToFile() {
+      String script = "library(grDevices)\n" +
+         "library(graphics)\n" +
+         "library('org.renjin.cran:DBI')\n" +
+         "\n" +
+         "sysname <- tolower(Sys.info()['sysname'])\n" +
+         "if(startsWith(sysname, 'windows')) {\n" +
+         "    fileName <- 'c:/tmp/svgplot.svg'\n" +
+         "} else {\n" +
+         "    fileName <- '/tmp/svgplot.svg'\n" +
+         "}\n" +
+         "\n" +
+         "# First plot\n" +
+         "svg(fileName)\n" +
+         "plot(sin, -pi, 2*pi)\n" +
+         "\n" +
+         "dev.off()\n" +
+         "stopifnot(file.exists(fileName))";
+
+
+      RenjinScriptEngineFactory factory = new RenjinScriptEngineFactory();
+      RenjinScriptEngine engine = factory.getScriptEngine();
+      System.out.println("Running script: \n" + script);
+      try {
+         engine.eval(script);
+      } catch (ScriptException | EvalException e) {
+         System.err.println("Failed to execute script");
+         e.printStackTrace();
+         fail(e.toString());
+      }
+   }
+}
